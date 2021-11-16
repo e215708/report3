27d8d029f022d962dd074aab09240bb087a45d10
SK <s@SKnoMacBook-Air.local>
2021-11-16 23:38:37 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215708/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215708/Hero.java
@@ -1,27 +1,25 @@
 package jp.ac.uryukyu.ie.e215708;
 
 /**
- * 敵クラス。
+ * ヒーロークラス。
  *  String name; //敵の名前
  *  int hitPoint; //敵のHP
  *  int attack; //敵の攻撃力
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
+public class Hero {
     private String name;
     private int hitPoint;
     private int attack;
     private boolean dead;
-    
-
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
+     * @param name ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack ヒーローの攻撃力
      */
-    public Enemy (String name, int maximumHP, int attack) {
+    public Hero (String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
         this.attack = attack;
@@ -30,16 +28,14 @@ public class Enemy {
     }
 
     /**
-     * Heroへ攻撃するメソッド。
+     * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
+     * @param e 攻撃対象
      */
-    public void attack(Hero hero){
-        if(hitPoint>0) {
+    public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
-    }
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
+        e.wounded(damage);
     }
 
     /**
@@ -51,26 +47,26 @@ public class Enemy {
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
     }
     /**
      * 名前を
-     * @return　Enemyの名前
+     * @return　Heroの名前
      */
     public String getName(){
         return name;
     }
     /**
      * HPを取得するゲッター
-     * @return　EnemyのHP
+     * @return　HeroのHP
      */
     public int getHitPoint(){
         return hitPoint;
     }
     /**
      * 攻撃値を取得するゲッター
-     * @return　Enemyの攻撃値
+     * @return　Heroの攻撃値
      */
     public int getAttack(){
         return attack;
@@ -111,5 +107,3 @@ public class Enemy {
         this.dead = dead;
     }
 }
-
-
